a3ddb7ad4e075c13ac483f10c3863e8f2ff28b01
Takayuki Miyahara <voyager.3taka28@gmail.com>
2025-06-15 23:01:04 +0900

    chore: add codegen.rs

---
+++ src/engine/codegen.rs
@@ -0,0 +1,161 @@
+use super::{parser::AST, Instruction};
+use crate::helper::safe_add;
+use std::{
+    error::Error,
+    fmt::{self, Display},
+};
+
+#[derive(Debug)]
+pub enum CodeGenError {
+    PCOverflow,
+    FailStar,
+    FailOr,
+    FailQuestion,
+}
+
+impl Display for CodeGenError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "CodeGenError: {:?}", self)
+    }
+}
+
+impl Error for CodeGenError {}
+
+#[derive(Default, Debug)]
+struct Generator {
+    pc: usize,
+    insts: Vec<Instruction>,
+}
+
+impl Generator {
+    fn gen_expr(&mut self, ast: &AST) -> Result<(), CodeGenError> {
+        match ast {
+            AST::Char(c) => self.gen_char(*c)?,
+            AST::Or(e1, e2) => self.gen_or(e1, e2)?,
+            AST::Plus(e) => self.gen_plus(e)?,
+            AST::Star(e) => self.gen_star(e)?,
+            AST::Question(e) => self.gen_question(e)?,
+            AST::Seq(v) => self.gen_seq(v)?,
+        }
+
+        Ok(())
+    }
+
+    fn inc_pc(&mut self) -> Result<(), CodeGenError> {
+        safe_add(&mut self.pc, &1, || CodeGenError::PCOverflow)
+    }
+
+    fn gen_seq(&mut self, exprs: &[AST]) -> Result<(), CodeGenError> {
+        for e in exprs {
+            self.gen_expr(e)?;
+        }
+
+        Ok(())
+    }
+
+    fn gen_char(&mut self, c: char) -> Result<(), CodeGenError> {
+        let inst = Instruction::Char(c);
+        self.insts.push(inst);
+        self.inc_pc()?;
+        Ok(())
+    }
+
+    fn gen_question(&mut self, e: &AST) -> Result<(), CodeGenError> {
+        // split L1, L2
+        let split_addr = self.pc;
+        self.inc_pc()?;
+        let split = Instruction::Split(self.pc, 0); // self.pcがL1, L2を仮に0と設定
+        self.insts.push(split);
+
+        // L1: eのコード
+        self.gen_expr(e)?;
+
+        // L2の値を設定
+        if let Some(Instruction::Split(_, l2)) = self.insts.get_mut(split_addr) {
+            *l2 = self.pc;
+            Ok(())
+        } else {
+            Err(CodeGenError::FailQuestion)
+        }
+    }
+
+    fn gen_star(&mut self, e: &AST) -> Result<(), CodeGenError> {
+        // L1: split L2, L3
+        let l1 = self.pc;
+        self.inc_pc()?;
+
+        let split = Instruction::Split(self.pc, 0);
+        self.insts.push(split);
+
+        // L2: eのコード
+        self.gen_expr(e)?;
+
+        // jump l1
+        self.inc_pc()?;
+        self.insts.push(Instruction::Jump(l1));
+
+        // L3の値を設定
+        if let Some(Instruction::Split(_, l3)) = self.insts.get_mut(l1) {
+            *l3 = self.pc;
+            Ok(())
+        } else {
+            Err(CodeGenError::FailStar)
+        }
+    }
+
+    fn gen_plus(&mut self, e: &AST) -> Result<(), CodeGenError> {
+        // L1: eのコード
+        let l1 = self.pc;
+        self.gen_expr(e)?;
+
+        // split L1, L2
+        self.inc_pc()?;
+        let split = Instruction::Split(l1, self.pc); // self.pcがL2
+        self.insts.push(split);
+
+        Ok(())
+    }
+
+    /// OR 演算子のコード生成器
+    ///
+    /// 以下のようなコードを生成
+    /// ```text
+    ///     split L1, L2
+    /// L1: e1のコード
+    ///    jump L3
+    /// L2: e2のコード
+    /// L3:
+    /// ```
+    fn gen_or(&mut self, e1: &AST, e2: &AST) -> Result<(), CodeGenError> {
+        // split L1, L2
+        let split_addr = self.pc;
+        self.inc_pc()?;
+        let split = Instruction::Split(self.pc, 0); // L1 = self.pc, L2は仮に0と設定
+        self.insts.push(split);
+
+        // L1: e1のコード
+        self.gen_expr(e1)?;
+        // jmp L3
+        let jmp_addr = self.pc;
+        self.insts.push(Instruction::Jump(0)); // L3は仮に0と設定
+
+        // L2の値を設定
+        self.inc_pc()?;
+        if let Some(Instruction::Split(_, l2)) = self.insts.get_mut(split_addr) {
+            *l2 = self.pc; // L2のアドレスを設定
+        } else {
+            return Err(CodeGenError::FailOr);
+        }
+
+        // L2: e2のコード
+        self.gen_expr(e2)?;
+        // L3の値を設定
+        if let Some(Instruction::Jump(l3)) = self.insts.get_mut(jmp_addr) {
+            *l3 = self.pc; // L3のアドレスを設定
+        } else {
+            return Err(CodeGenError::FailOr);
+        }
+
+        Ok(())
+    }
+}
